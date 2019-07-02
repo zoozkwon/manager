98fa87be95d4af0fbc46f2817ae5331a3800c572
zoozkwon <zooz@amuz.co.kr>
2019-07-02 10:23:37 +0900

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ShopManagerModule } from '../../projects/Shop-Manager/src/app/app.module';
+import { ShopMasterModule } from '../../projects/Shop-Master/src/app/app.module';
+
+const routes: Routes = [
+  {
+    path: 'manager',
+    loadChildren: '../../projects/Shop-Manager/src/app/app.module#ShopManagerModule'
+  },
+  {
+    path: 'master',
+    loadChildren: '../../projects/Shop-Master/src/app/app.module#ShopMasterModule'
+  },
+  { path: '**', redirectTo: '/manager/login' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    ShopManagerModule.forRoot(),
+    ShopMasterModule.forRoot(),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { sanitizeHtmlPipe } from './sanitize-html.pipe';
+
+import { ShopManagerModule } from '../../projects/Shop-Manager/src/app/app.module';
+import { ShopMasterModule } from '../../projects/Shop-Master/src/app/app.module';
+
 @NgModule({
   declarations: [
+    sanitizeHtmlPipe,
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ShopManagerModule.forRoot(),
+    ShopMasterModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
